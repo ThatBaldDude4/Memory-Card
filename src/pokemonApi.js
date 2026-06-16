81a07f7b2ce843d6fd6b0448ee98a44792a95041
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-16 09:01:40 -0500

    add api connection
    function takes array of names and fetches
    corresponding data. Data is parsed to only include
    name and image

---
+++ src/pokemonApi.js
@@ -0,0 +1,16 @@
+export default function getPokemonData(pokemonArray) {
+  const pokemonPromises = pokemonArray.map((name) => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => res.json())
+      .then((data) => parseData(data));
+  });
+
+  return Promise.all(pokemonPromises);
+}
+
+function parseData(data) {
+    return {
+        name: data.name,
+        image: data.sprites.front_default
+    };
+}
