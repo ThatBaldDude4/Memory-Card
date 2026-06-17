f5e7c35b183bb3d47180400a8730a16bc30ff597
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-16 20:08:59 -0500

    move score state into child

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
 import {useState} from 'react'
-import CardsContainer from './CardsContainer'
+import Game from './CardsContainer'
 
 function App() {
-  const [bestScore, setBestScore] = useState(0);
-  const [currentScore, setCurrentScore] = useState(0);
-
-  function handleResetGame() {
-    setCurrentScore(0)
-  }
-
   return (
-    <>
-      <p>Best Score: {bestScore}</p>
-      <p>Current Score: {currentScore}</p>
-      <CardsContainer 
-        bestScore={bestScore}
-        currentScore={currentScore}
-        setBestScore={setBestScore}
-        setCurrentScore={setCurrentScore}
-        handleResetGame={handleResetGame}
-      />
-    </>
+    <Game/>
   )
 
 }
