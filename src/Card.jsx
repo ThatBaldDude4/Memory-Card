cef59e68bbab1c1adf4a69c6dbc94644ae443639
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-17 22:39:59 -0500

    images are randomly rotated on every render

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,14 @@
-export default function Card({name, image, onClick}) {
+import { useState } from "react"
+
+export default function Card({name, image, onClick, rotation}) {
+    const customStyle = {
+        transform: `rotate(${rotation}deg)`,
+    }
+    
     return (
         <div className="pokemon-card" onClick={() => {onClick()}}>
             <h2>{name}</h2>
-            <img src={image} />
+            <img src={image} style={customStyle}/>
         </div>
     )
 }
