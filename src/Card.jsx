60f378d7ae35c0d7bcbbdc61d961ec103931071c
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-23 08:55:56 -0500

    add aria role to card containers

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,13 +4,20 @@ export default function Card({name, image, onClick, rotation, isHardMode}) {
     // if hard mode apply unique styles
     const customStyle = !isHardMode ? {} : {
         transform: `scale(0.5) rotate(${rotation}deg)`,
+        border: `none`,
+        fontSize: `10px`,
+        margin: `0`,
+        padding: `0`,
+    }
 
+    const imageStyle = !isHardMode ? {} : {
+        backgroundColor: `white`,
     }
 
     return (
-        <div className="pokemon-card" onClick={() => {onClick()}} style={customStyle}>
+        <div role="button" className="pokemon-card" onClick={() => {onClick()}} style={customStyle}>
             <h2>{name}</h2>
-            <img src={image} />
+            <img src={image} style={imageStyle} alt={`${name} pokemon`} />
         </div>
     )
 }
